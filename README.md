064dd04697c7ab6e805ad9430c568d62a5fc8f01
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-02-15 21:31:30 -0800

    Attempting again
    - Set up project with check boxes and input limits on the webpage (not browser prompts yet)
    -ids for all complex character types (lowercase, uppercase, numbers, special characters)
    -started length function

--- README.md
+++ README.md
@@ -18,3 +18,6 @@ WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
+
+
+Attempting again using different video tutorials https://www.youtube.com/watch?v=iKo9pDKKHnc 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,38 @@
 // Assignment code here
 
 
+// starting with versions as explained in webdev video, will need to transition to meet assingnment prompts
+// 1st user step is define length
+// 4 Password components: Uppercase, lowercase, numbers, special characters
+//
+// upperLetters = “ABCDEFGHIJKLMNOPSQRSTUVWXYZ”;
+// lowerLetters = “abcdefghijklmnopqrstuvwxyz”;
+// numbers = “0123456789”;
+// specialCharacters = “!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+//parameters password length
+const characterAmountRange = document.getElementById
+const form = document.getElementById('passwordGeneratorForm')
+
+characterAmountNumber.addEventlistener('input', syncCharacterAmount)
+characterAmountNumber.addEventlistener('input', syncCharacterAmount)
+
+//listen for form submission
+form.addEventListener('submit', e => {
+  e.preventDefault()
+})
+
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+
+// came with assignment ---------------
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
