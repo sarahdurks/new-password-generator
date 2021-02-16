36a12c17d0b0ebc7cf71a28aa7419a252a5e5e18
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-02-15 22:50:19 -0800

    deleting original code in prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,20 +77,3 @@ function syncCharacterAmount(e) {
   characterAmountRange.value = value
 }
 
-// came with assignment ---------------
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
