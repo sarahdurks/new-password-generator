15e7f4da69203738894e15fcfaef627de11a6be6
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-02-16 16:07:24 -0800

    Code still pretty broken
    tried to tie password variables to JS Prompts, logging when "true"
    
    Have no listed arrays out,use the ASCII ranges

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,54 @@
 // Assignment code here
 
+// VALIDATE the input for number
+
+// LENGTH
+//const aNumber = Number(window.prompt("Type a number", ""));
+// https://stackoverflow.com/questions/15047140/javascript-prompt-number-and-continue-prompting-if-answer-is-wrong not sure this is right
+
+characterAmountNumber = prompt("How many characters do you want toinclude in your password? Must between 8-128.","8");
+
+function promptcharacterAmountNumber( text)
+{
+    if(text == '' ){
+     text = "Please enter a number from 8 - 128";   
+    }
+    var number = parseInt(window.prompt(text, ""));
+    checkNumber(number);
+
+}
+function checkNumber(){
+if (number <= 128 && number >= 8) {
+    document.write("Your number (" + number + ")  matches requirements", "");
+} else if (isNaN(number)) {
+    promptForNumber("Your submission (" + number + ") doesn't meet criteria. It is not between 8 and 128", "");
+}
+
+// DEFINE PASSWORD CRIERIA FOR UPPERCASE,LOWERCASE, NUMBERS, SPECIAL
+
+// Trying to "keep" value user inputs if true to later include that character tyoe in  password. not sure that is correct approach
+
+
+var includeUppercase = prompt("Do you want to include Upper case letters in your password?");
+if(includeUppercase)
+{document.getElementById(“includeUppercase”);} else {console.log(“”);} 
+
+var includeLowercase = prompt("Do you want to include Lower case letters in your password?");
+if(includeLowercase)
+  {document.getElementById((“includeLowercase”);} else { console.log(“”);}
+
+var includeNumbers = prompt ("Do you want to include Numbers in your password?");
+if(includeNumbers)
+{document.getElementById(“includeNumbers); } else { console.log(“”); }
+
+var includeSpecial = prompt ("Do you want to include Special Characters (e.g., !@#$%) in your password?");
+if(includeSpecial)
+{document.getElementById(“includeSpecial”); } else { console.log(“”); }
+
+//if a user wants allof the above things in the password, there should be an ID for each type we use later
+
 
 // starting with versions as explained in webdev video, will need to transition to meet assingnment format
-// 1st user step is define length
-// 4 Password components: Uppercase, lowercase, numbers, special characters
 //
 // upperLetters = “ABCDEFGHIJKLMNOPSQRSTUVWXYZ”;
 // lowerLetters = “abcdefghijklmnopqrstuvwxyz”;
@@ -11,36 +56,43 @@
 // specialCharacters = “!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // constants
-const characterAmountNumber = document.getElementById ('characterAmountNumber')
-const IncludeUppercaseElement = document.getElementById ('includeUppercase')
-const IncludeLowercaseElement = document.getElementById ('includeLowercase')
-const IncludeNumbersElement = document.getElementById ('includeNumbers')
-const IncludeSpecialElement = document.getElementById ('includeSpecial')
+/* This part takes dynamic T/F value from  user input in prompts and translates it into a constant*/
+const characterAmountNumber = document.getElementById ('characterAmountNumber') // Length set by user
+const IncludeUppercaseElement = document.getElementById ('includeUppercase') //  If user includes uppercase
+const IncludeLowercaseElement = document.getElementById ('includeLowercase') // If user includes lowercase
+const IncludeNumbersElement = document.getElementById ('includeNumbers') // if user inlcudes numbers
+const IncludeSpecialElement = document.getElementById ('includeSpecial') // if user inlcudes  special characters
 
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay') 
+const form = document.getElementById('passwordGeneratorForm') // maps to form on index.html
+const passwordDisplay = document.getElementById('passwordDisplay') // maps to where password will update and show
 
 
-characterAmountNumber.addEventlistener('input', syncCharacterAmount)
-
-//listen for form submission
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  const characterAmount = characterAmountNumber.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeLowercase = includeLowercaseElement.checked
-  const includeNumbers = includeuNumbersElement.checked
-  const includeSpecial = includeSpecialElement.checked
+characterAmountNumber.addEventlistener('input', syncCharacterAmount) //takes number we finally accept from first prompt in the right range
+// May need thisbut if receiving prompts above maybe not. This is the "form version from video" 
+//listen for form submission - needsto change to go to prompts
+//form.addEventListener('submit', e => {
+ // e.preventDefault()
+ // const characterAmount = characterAmountNumber.value
+ // const includeUppercase = includeUppercaseElement.checked
+ // const includeLowercase = includeLowercaseElement.checked
+  // const includeNumbers = includeuNumbersElement.checked
+ // const includeSpecial = includeSpecialElement.checked
+  
+  
   const password = generatePassword(characterAmount,includeUppercase, includeLowercase, includeNumbers, includeSpecial)
   passwordDisplay.innerText = password
 })
-//see https://theasciicode.com.ar/
+
+
+
+// do not actually need to list these out, can tie them to a constant representing long list
+// see https://theasciicode.com.ar/ American Standard Code for Information Interchange it maps to ranges below
 const UPPERCASEE_CHAR_CODES = arrayFromLowToHigh(65,90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97,122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48,57
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58,64), .concat(arrayFromLowToHigh(91,96), .concat(arrayFromLowToHigh(123,126)
 )
-
+//will need to make true to question = include
 
 function generatePassword (characterAmount,includeUppercase, includeLowercase, includeNumbers, includeSpecial){
   let charCodes =LOWERCASE_CHAR_CODES
@@ -48,16 +100,12 @@ function generatePassword (characterAmount,includeUppercase, includeLowercase, i
   if (includeUppercase) charCodes = charCodes.concat (NUMBER_CHAR_CODES)
   if (includeUSymbol) charCodes = charCodes.concat (SYMBOL_CHAR_CODES)
 
-  const passwordCharacters = []
+const passwordCharacters = []
 for (let i = 0; i < characterAmount, i++) {
   const characterCode = charCodes [Math.floor(Math.random()* charCodes.length)]
 passwordCharacters.push(String.fromCharCode(charCodes))
 return passwordCharacters.join('')
-
-
 }
-
-//array was wrong approach last time- super inefficient see above overall list better looped thru
 
 function arrayFromLowtoHigh, high) {
   for )let i = low; i <= high; i++) {
@@ -65,11 +113,9 @@ function arrayFromLowtoHigh, high) {
   }
   array.push(i)
 
-
 String.fromCharCode(65)
 
 }
-
 
 function syncCharacterAmount(e) {
   const value = e.target.value
