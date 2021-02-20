4d4710f58db4b9cbdf75f3083f2ec28e295301b0
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-02-20 15:45:19 -0800

    back to form input version
    - very stuck on JS prompt >> validating input behavior so moving back towards form version like in YT video with event listeners
    - things still not connected/some half leftovers
    - HTML needs to be updated w CSS formatting to streamline
    // kept line from earlier iteration about validation checking (needing a complex category) even though not set up yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,45 @@
 
-// State variablees, make them equivalent to password components created by prompts (if done right)
-var CharachterAmountNumber = document.getElementById("CharacterAmountNumber");
-var lncludeLowecase = document.getElementById("lncludeLowecase");
-var lncludeUppercase = document.getElementById("lncludeLowecase");
-var includeNumbers = document.getElementById("includeNumbers");
-var includeSpecial = document.getElementById("includeSpecial");
+const charachterLength = document.getElementById
+('charachterLength')
+ const charachterUppercase = document.getElementById
+ ('charachterUppercase')
+ const charachterLowercase = document.getElementById
+ ('charachterLowercase')
+ const charachterNumbers = document.getElementById
+ ('charachterNumbers')
+ const charachterSpecial = document.getElementById
+ ('charachterSpecial')
 
 
-// Series of prompts to define criteria
-function definePassword() {
-    var characterAmountNumber = prompt("Please enter a number between 8 and 128 for your password length."));
-do (isNaN(characterAmountNumber) || characterAmountNumber > 128 || characterAmountNumber < 8);
-    while (characterAmountNumber > 7 && characterAmountNumber < 129) 
-    
-    consolelog(parseInt(characterAmountNumber))
-//?? how to save value differently as number
-    var charachterTypes = function () {
-        includeLowercase = confirm("Would you like lowercase characters?")
-        if true == document.createElement('includeLowecase', {id:"includeLowecase"});
+ 
+//links form around inputs to a constant, used for submit event listener
+const form = document.getElementbyId(passwordGeneratorForm)
 
-        includeUppercase = confirm("Would you like uppercase characters?")
-        if true == document.createElement('includeUppercase', {id:"includeUppercase"});
+//event listener for user input on password length
+charachterLength.addEventListener('input', inputCharachterLength)
 
-        includeNumbers = confirm("Would you like numeric characters?")
-        if true == document.createElement('includeNumbers', {id:"includeNumbers"});
+//takes event of user inputting # translates to value used for length
+function inputCharachterLength(e) {
+    const value = e.target.value
+    charachterLength.value = value
 
-        includeSpecial = confirm("Would you like special characters?")
-        if true == document.createElement('includeSpecial', {id:"iincludeSpecial"});
+    //prevent refresh of page clearing inputs when user hits generate password
+    form.addEventListener('submit', e => (
+        e.preventDefault()
+        //translating input state (t/f) in HTML / from user to constants to use in password composition
+        const passwordLength = charachterLength.value
+        const includeUppercase = charachterUppercaseElement.checked 
+        const includeLowercase = charachterLowercaseElement.checked 
+        const includeNumbers = charachterNumbersElement.checked 
+        const includeSpecial = charachterSpecialElement.checked 
+        const password = generatePassword(passwordLength, includeUppercase, includeLowercase, includeSpecial)
+    )
+}
 
 // need to limit someone progressing if they dont make password complicated, not sure about looping them back
 if (includeLowercase === false && includeUppercase === false && includeSpecial === false && includeNumbers === false) {
     alert("At least one complex password component must be selected, please try again.")
-
-
-// DOCUMENTED USER INPUTS >> CONSTANTS
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const IncludeUppercaseElement = document.getElementById('includeUppercase')
-const IncludeLowercaseElement = document.getElementById('includeLowercase')
-const IncludeNumbersElement = document.getElementById('includeNumbers')
-const IncludeSpecialElement = document.getElementById('includeSpecial')
+    var charachterTypes = function () {
 
 
 // FORM AND PASSWORD DISPLAY TO IDs
@@ -48,19 +49,20 @@ const passwordDisplay = document.getElementById('passwordDisplay')
 const password = generatePassword(characterAmountNumber, includeUppercase, includeLowercase, includeNumbers, includeSpecial)
 passwordDisplay.innerText = password
 
+
+
 // American Standard Code for Information Interchange it maps to ranges below https://theasciicode.com.ar/ from YT video
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
 const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64), .concat(arrayFromLowToHigh(91, 96), .concat(arrayFromLowToHigh(123, 126))
 
-            (characterAmountNumber, includeUppercase, includeLowercase, includeNumbers, includeSpecial) => {
+            (characterAmountNumber, includeLowercase, includeUppercase, includeNumbers, includeSpecial) => {
                 let charCodes = LOWERCASE_CHAR_CODES; //why does this not map to includelowercase? also different order than listed in funciton above
                 if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
                 if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
                 if (includeSpecial) charCodes = charCodes.concat(SPECIAL_CHAR_CODES);
             }
-            // rewatch video for below
             const passwordCharacters = []
             for (let i = 0; i < characterAmount, i++) {
                 const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
@@ -76,8 +78,4 @@ const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(
             String.fromCharCode(65)
         }
 
-        function syncCharacterAmount(e) {
-            const value = e.target.value
-            characterAmountNumber.value = value
-            characterAmountRange.value = value
-        }
+     
