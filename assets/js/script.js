03a61515a34bf8e00427415fe8d6a4fe245dd589
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-02-20 17:31:42 -0800

    found a few bracket typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const charachterUppercase = document.getElementById('charachterUppercase')
 const charachterLowercase = document.getElementById('charachterLowercase')
 const charachterNumbers = document.getElementById('charachterNumbers')
 const charachterSpecial = document.getElementById('charachterSpecial')
-const form = document.getElementbyId('passwordGeneratorForm') // form in total
+const form = document.getElementbyId('passwordForm') // form in total
 const passwordDisplay = document.getElementById('passwordDisplay') //displayed password
 
 // Arrays for "types" - rather than the a,b,c list - used in video https://theasciicode.com.ar/ 
@@ -21,7 +21,7 @@ const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(
 charachterLength.addEventListener('input', inputCharachterLength)
 
 // prevent refresh of page clearing inputs when user hits generate password (default behavior for event)
-form.addEventListener('submit', e => (
+form.addEventListener('submit', e => {
         e.preventDefault()
         // translating input state (t/f) in HTML / from user to constants to use in password composition
         const passwordLength = charachterLength.value
@@ -30,30 +30,29 @@ form.addEventListener('submit', e => (
         const includeNumbers = charachterNumbersElement.checked
         const includeSpecial = charachterSpecialElement.checked
         const password = generatePassword(passwordLength, includeUppercase, includeLowercase, includeSpecial)
-        passwordDisplay.innerText = password
+        passwordDisplay.innerText = password //maybe where things are breaking?
 })
 
 /*need to limit someone progressing if they dont make password complicated, not sure about looping them back
 if (includeLowercase === false && includeUppercase === false && includeSpecial === false && includeNumbers === false) {
     alert("At least one complex password component must be selected, please try again.")*/
 
-function generatePassword (passwordLength, includeUppercase, includeLowercase, includeSpecial)
-{
-let charCodes = LOWERCASE_CHAR_CODES; 
-
-if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
-if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
-if (includeSpecial) charCodes = charCodes.concat(SPECIAL_CHAR_CODES);
+function generatePassword(passwordLength, includeUppercase, includeLowercase, includeSpecial){
+let charCodes = LOWERCASE_CHAR_CODES 
+if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+if (includeSpecial) charCodes = charCodes.concat(SPECIAL_CHAR_CODES)
 }
 
 const passwordCharacters = []
-for (let i = 0; i < , i++) {
+for (let i = 0; i < passwordLength; i++) {
 const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
 passwordCharacters.push(String.fromCharCode(charCodes))
+}
 return passwordCharacters.join('')
-                    }
+}
 
-function arrayFromLowtoHigh, high) {
+function arrayFromLowtoHigh(low, high) {
   const array = []
 for (let i = low; i <= high; i++) {
   array.push(i)
@@ -64,5 +63,4 @@ return array
 function inputCharachterLength(e) {
   const value = e.target.value
   charachterLength.value = value
-
-                }
+}
