97df8dea205f4f7ce2b8262bd6073d6ff08dd541
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-02-20 20:15:45 -0800

    still not working so pushing a version with simplified naming conventions, hoping to catch issue after a pause

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,70 @@
-// password components mapped to DOM ids per user input (checkboxes) of criteria
-const charachterLength = document.getElementById('charachterLength')
-const charachterUppercaseElement = document.getElementById('charachterUppercase')
-const charachterLowercaseElement = document.getElementById('charachterLowercase')
-const charachterNumbersElement = document.getElementById('charachterNumbers')
-const charachterSpecialElement = document.getElementById('charachterSpecial')
-const form = document.getElementbyId('passwordForm') // form in total
-const passwordDisplay = document.getElementById('passwordDisplay') //displayed password
+//References items in DOM - creates constant for input of password criteria
+const charLengthVal = document.getElementById('charLength')
+const charUpperEl = document.getElementById('charUpper')
+const charLowerEl = document.getElementById('charLower')
+const charNumsEl = document.getElementById('charNums')
+const charSpecialEl = document.getElementById('charSpecial')
+const form = document.getElementbyId('passwordForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+
+//This isn't working yet, but in theory should listen and create a new constant based on state of input (e.g., if special checked, pSpecial created)
+// Should also prevent default of losing input/refreshing, and convert length input to a value from string
+charLength.addEventListener('input', charachterLengthfx)
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	const pLength = charLengthVal.value
+	const pUpper = charUpperEl.checked
+	const pLower = charLowerEl.checked
+	const pNumbers = charNumsEl.checked
+	const pSpecial = charSpecialEl.checked
+	const password = generatePassword(pLength, pUpper, pLower, pNumbers, pSpecial)
+	passwordDisplay.innerText = password //maybe where things are breaking?
+})
+
+//Don't yet have validation of missing criteria if no boxes selected + warning
 
 // Arrays for "types" - rather than the a,b,c list - used in video https://theasciicode.com.ar/ 
 // American Standard Code for Information Interchange it maps to ranges below 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
-
-
-
-// event listener for user input on password length, translating to value
-// has to be handled differently like parseInt in prompt approach, to get number
-charachterLength.addEventListener('input', charachterLengthfx)
-
-// prevent refresh of page clearing inputs when user hits generate password (default behavior for event)
-form.addEventListener('submit', e => {
-        e.preventDefault()
-        // translating input state (t/f) in HTML / from user to constants to use in password composition
-        const passwordLength = charachterLength.value
-        const includeUppercase = charachterUppercaseElement.checked
-        const includeLowercase = charachterLowercaseElement.checked
-        const includeNumbers = charachterNumbersElement.checked
-        const includeSpecial = charachterSpecialElement.checked
-        const password = generatePassword(passwordLength, includeUppercase, includeLowercase, includeSpecial)
-        passwordDisplay.innerText = password //maybe where things are breaking?
-})
-
-/*need to limit someone progressing if they dont make password complicated, not sure about looping them back
-if (includeLowercase === false && includeUppercase === false && includeSpecial === false && includeNumbers === false) {
-    alert("At least one complex password component must be selected, please try again.")*/
-
-function generatePassword(passwordLength, includeUppercase, includeLowercase, includeSpecial){
-let charCodes = LOWERCASE_CHAR_CODES 
-if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-if (includeSpecial) charCodes = charCodes.concat(SPECIAL_CHAR_CODES)
-
-const passwordCharacters = []
-for (let i = 0; i < passwordLength; i++) {
-   const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-   passwordCharacters.push(String.fromCharCode(characterCode))
-}
-return passwordCharacters.join('')
+const UPPER_CHAR = arrayRange(65, 90)
+const LOWER_CHAR = arrayRange(97, 122)
+const NUM_CHAR = arrayRange(48, 57)
+const SPECIAL_CHAR = arrayRange(33, 47).concat(
+	arrayRange(58, 64)
+).concat(
+	arrayRange(91, 96)
+).concat(
+	arrayRange(123, 126)
+)
+// function goes through and grabs from refrenced ranges
+function arrayRange(low, high) {
+	const array = []
+	for (let i = low; i <= high; i++) {
+		array.push(i)
+	}
+	return array
 }
 
-function arrayFromLowtoHigh(low, high) {
-  const array = []
-for (let i = low; i <= high; i++) {
-  array.push(i)
-}
-return array
+//Puts together what is returned for each type
+function generatePassword(pLength, pLower, pUpper, pNumbers, pSpecial) {
+	let (pLower) charCodes = LOWER_CHAR // how do I make this not weird / understand first charcodes is pLower
+	if (pUpper) charCodes = charCodes.concat(UPPER_CHAR)
+	if (pNumbers) charCodes = charCodes.concat(NUM_CHAR)
+	if (pSpecial) charCodes = charCodes.concat(SPECIAL_CHAR)
+
+
+
+	//output of additive/concat categories for password, should display for user
+	const pwChar = []
+	for (let i = 0; i < pLength; i++) {
+		const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+		pwChar.push(String.fromCharCode(characterCode))
+	}
+	return pwChar.join('')
 }
 
-function charachterLengthfx(e) {
-  const value = e.target.value
-  charachterLength.value = value
+//there may be an easier way to do this since not keeping twp inputoptions for #s, like parseInt in prompts
+function charLengthfx(e) {
+	const value = e.target.value
+	charLength.value = value
 }
